b55524c08c7a08bb53cf81d58446c32380b088df
Nitish Kumar <nitish.kumar@flodataanalytics.com>
2025-01-16 14:39:20 +0530

    localtest

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ proxy.on("proxyRes", (proxyRes, req, res) => {
 
         // Replace 'gis.siriuspower.co.za' with 'localhost:3000' in the response body
         let modifiedBody = bodyText.replace(
-          /https:\/\/gis\.siriuspower\.co\.za\/geoserver/g,
+          /https:\/\/geoserver\.flodataanalytics\.com\/geoserver/g,
           "https://geoserver-porxy.onrender.com/geoserver"
         );
         modifiedBody = modifiedBody.replace(
@@ -69,7 +69,7 @@ proxy.on("proxyRes", (proxyRes, req, res) => {
           `geoserver/openlayers3/ol.js?username=${queryUsername}&password=${queryPassword}`
         );
         modifiedBody = modifiedBody.replace(
-          /gis\.siriuspower\.co\.za/g,
+          /geoserver\.flodataanalytics\.com/g,
           `geoserver-porxy.onrender.com`
         );
 
